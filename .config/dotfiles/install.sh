6970e046be2232e4e91626a9589e0f05258973bb
Micah Snell <snellmicah@gmail.com>
2024-03-14 22:02:48 -0500

    fix: dont copy readme, and appropriately update submodules

--- .config/dotfiles/install.sh
+++ .config/dotfiles/install.sh
@@ -7,9 +7,11 @@ gitDir="$(cat ../../.git | cut -d ' ' -f2)"
 copyTo="$(dirname $gitDir)/"
 copyFrom="$(realpath ../../)/"
 echo "Copying the contents of $copyFrom to $copyTo"
-rsync --recursive --links --exclude ".git" "$copyFrom" "$copyTo"
+rsync --recursive --links --exclude ".git" --exclude README "$copyFrom" "$copyTo"
 
+# initialize submodules, don't show untracked files for the dotfiles repo
 echo "Initializing $gitDir"
+cd "$copyTo"
 git --git-dir="$gitDir" --work-tree="$copyTo" submodule update --init --recursive "$copyTo"
 git --git-dir="$gitDir" --work-tree="$copyTo" config status.showUntrackedFiles no
 
