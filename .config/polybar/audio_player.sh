fd95b8fe3709a363bd4f846d395e04813c59f1d2
Micah Snell <snellmicah@gmail.com>
2024-03-23 16:41:41 -0500

    style: apply common style to bash scripts

--- .config/polybar/audio_player.sh
+++ .config/polybar/audio_player.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#
+# outputs icon and song name of current song on spotify, depending on status
+#
+# args: none
 
 status="$(playerctl --player=spotify status 2>/dev/null)"
 title="$(playerctl --player=spotify metadata title | sed 's/ -.*//; s/ (.*//')"
 
-if [ "$status" = "Playing" ]; then
+if [ "${status}" = "Playing" ]; then
   echo -e "♫ $title"
-elif [ "$status" = "Paused" ]; then
+elif [ "${status}" = "Paused" ]; then
   echo -e "■ $title"
 else
   echo "idle"
