5d199d7e6e7ba2d8bcfdb2637477e6d6f58d3682
Micah Snell <snellmicah@gmail.com>
2024-04-06 17:16:48 -0500

    fix: improve parsing of hostname from ssh command, remove arguments, username, and config options

--- .config/tmux/right_status.sh
+++ .config/tmux/right_status.sh
@@ -8,7 +8,12 @@
 cd "$3" || exit 1
 gitInfo="$(gitmux -cfg ${HOME}/.config/tmux/gitmux.conf) "
 
-remoteHost="$(ps -t $1 -o ppid,args 2>/dev/null | awk '/'"$2"' ssh/{print $NF}')"
+remoteHost=$(ps -t "$1" -o ppid,args 2>/dev/null |
+               grep "$2 ssh" |
+               sed -e 's/\s\+\-\+[0-9A-Za-z]\+//g' \
+                   -e 's/[0-9A-Za-z]\+@//' \
+                   -e 's/[0-9A-Za-z]\+=[0-9A-Za-z]\+//g' |
+               awk '{print $3}' | tr -d '\n')
 if [ -n "${remoteHost}" ]; then
   hostName="$(ssh -G "${remoteHost}" | awk '/^hostname /{sub (/\..*/, ""); print $NF}')"
   userName="$(ssh -G "${remoteHost}" | awk '/^user /{print $NF}')"
