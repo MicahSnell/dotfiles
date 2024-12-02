53c1f174c66611e5bbcc2976a4097a1a6a67a8cc
Micah Snell <snellmicah@gmail.com>
2024-12-01 22:21:09 -0600

    feat: use template files and envsubst to generate config files with current color settings

--- .local/bin/update_theme.sh
+++ .local/bin/update_theme.sh
@@ -8,29 +8,42 @@
 xrdb -load "${HOME}/.Xresources"
 
 # get foreground and background
-foreground="$(xrdb -get foreground)"
-background="$(xrdb -get background)"
+export FOREGROUND="$(xrdb -get foreground)"
+export BACKGROUND="$(xrdb -get background)"
 
 # add fixed amount to background to create alternate background
 highlight="1447453"
-sum="$(($((16${background})) + highlight))"
-background_alt="$(printf '#%x' ${sum})"
+sum="$(($((16${BACKGROUND})) + highlight))"
+export BACKGROUND_ALT="$(printf '#%x' ${sum})"
 
 # add it to Xresource data base
-xrdb -merge <(echo "background-alt: ${background_alt}")
+xrdb -merge <(echo "background-alt: ${BACKGROUND_ALT}")
 
-# update foreground and background in kitty
-sed -i "/^foreground\s\+/{s/.*/foreground ${foreground}/}" "${HOME}/.config/kitty/kitty.conf"
-sed -i "/^background\s\+/{s/.*/background ${background}/}" "${HOME}/.config/kitty/kitty.conf"
+# colors 0 through 15
+export COLOR_0="$(xrdb -get color0)"
+export COLOR_1="$(xrdb -get color1)"
+export COLOR_2="$(xrdb -get color2)"
+export COLOR_3="$(xrdb -get color3)"
+export COLOR_4="$(xrdb -get color4)"
+export COLOR_5="$(xrdb -get color5)"
+export COLOR_6="$(xrdb -get color6)"
+export COLOR_7="$(xrdb -get color7)"
+export COLOR_8="$(xrdb -get color8)"
+export COLOR_9="$(xrdb -get color9)"
+export COLOR_10="$(xrdb -get color10)"
+export COLOR_11="$(xrdb -get color11)"
+export COLOR_12="$(xrdb -get color12)"
+export COLOR_13="$(xrdb -get color13)"
+export COLOR_14="$(xrdb -get color14)"
+export COLOR_15="$(xrdb -get color15)"
 
-# update foreground, background, and background-alt in rofi
-sed -i "s/\s\+foreground:.*\;$/  foreground: ${foreground};/" "${HOME}/.config/rofi/style.rasi"
-sed -i "s/\s\+background:.*\;$/  background: ${background};/" "${HOME}/.config/rofi/style.rasi"
-sed -i "s/\s\+background-alt:.*\;$/  background-alt: ${background_alt};/" "${HOME}/.config/rofi/style.rasi"
+# generate config files from template files
+tmplFiles=(
+  "${HOME}/.config/kitty/kitty.conf.template"
+  "${HOME}/.config/rofi/style.rasi.template"
+)
 
-# update first sixteen colors in kitty and rofi
-for c in {0..15}; do
-  newColor="$(xrdb -get color${c})"
-  sed -i "/^color${c}\s\+/{s/.*/color${c} ${newColor}/}" "${HOME}/.config/kitty/kitty.conf"
-  sed -i "s/\s\+color${c}:.*\;$/  color${c}: ${newColor};/" "${HOME}/.config/rofi/style.rasi"
+for template in "${tmplFiles[@]}"; do
+  config="${template%.template}"
+  envsubst < "${template}" > "${config}"
 done
